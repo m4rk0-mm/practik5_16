7ad29a205747583b9c978b2d42cf6bbf50bcfe99
m4rk0-mm <markiyan27@outlook.com>
2022-04-11 22:10:34 +0300

    version2

--- src/com/company/fouth.java
+++ src/com/company/fouth.java
@@ -1,25 +1,37 @@
 package com.company;
 
-import java.rmi.MarshalException;
+
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class fouth {
     public static void main(String[] args) {
         int sprob = 0;
-        int prog;
-        while (sprob != 2){
-                prog = (int)(Math.random()*4);
-                    Scanner sc = new Scanner(System.in);
-                    System.out.println("Ведіть числа : ");
-                    sc.nextInt();
-                    sprob++;
-                    if(prog == sc.nextInt()){
-                        System.out.println("Вітаю ви вгадали ( : ");
-                        break;
-                    }
-                }
+        int[] num = new int[3];
+
+        for (int i = 0; i < num.length; i++) {
+            num[i] = (int) Math.round(Math.random() * 2 + 1);
+            System.out.println(num[i]);
+        }
+        int[] num_2 = new int[3];
+        while (sprob != 2) {
+            for (int a = 0; a < num_2.length; a++) {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Ведіть числа :");
+                num_2[a] = sc.nextInt();
+
             }
+            sprob++;
+            if (Arrays.equals(num, num_2)) {
+                System.out.println("---- Вітаю ви виграли !! ----");
+                break;
+            }else if (sprob == 2){
+                System.out.println("---- Повезе наступного разу ( :  ----");
+            }else System.out.println("---- Спробуй ще раз ----");
+
         }
 
 
+    }
+}
 
